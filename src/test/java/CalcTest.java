aea27a84420dbb11829d5e843c4142e97d79c475
ChristianNeal015 <cjdaking030@gmail.com>
2020-01-10 13:14:17 -0500

    pushed

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,14 @@
+public class CalcTest {
+//    @Test
+    public void addTest(){
+//        Calc calc = new Calc();
+//
+//        int x = 1;
+//        int y = 2;
+//
+//        int expected = 3;
+//
+//        //when
+//        int atcual = calc.add
+    }
+}
